0ee41693c7f7b6e3bcb1d949e789f606d4dff234
ka <ka.kaosf@gmail.com>
2013-11-25 10:55:27 +0900

    add snippet.clj

---
+++ snippet.clj
@@ -0,0 +1,32 @@
+; list 9-2
+
+(ns org.stuff.clojuroid.main)
+
+(use 'neko.resource 'neko.activity 'neko.notify 'neko.ui 'neko.threading 'neko.application)
+
+(on-ui
+  (set-content-view! a
+    (make-ui [:linear-layout {}
+              [:text-view {:text (str "Hello from Clojure!" (java.util.Date.))}]])))
+
+(defn some-callback [_]
+  (toast "finished" :short))
+
+;(on-ui
+;  (set-content-view! a
+;    (make-ui [:linear-layout {:layout-height :fill}
+;      [:button {:def hello :text "A Button" :on-click some-callback :enabled true}]])))
+(on-ui
+  (set-content-view! a
+    (make-ui [:linear-layout {:layout-height :fill}
+      [:button {:text "A Button" :on-click some-callback :enabled true}]])))
+
+(def icon-rsc
+  (get-resource :drawable :ic-launcher))
+
+(fire :new-mail
+      (notification :icon icon-rsc
+                    :ticker "You've got mail"
+                    :content-title "One new message"
+                    :content-text "FROM: foo@bar.com"
+                    :action [:activity "my.package.VIEW_MAIL"]))
