d5bb80fc2c2aba44f666680eb4c92ab82267fe65
ka <ka.kaosf@gmail.com>
2013-11-30 03:10:45 +0900

    create SubActivity (failure)

---
+++ src/clojure/org/stuff/clojuroid/sub.clj
@@ -0,0 +1,13 @@
+(ns org.stuff.clojuroid.sub
+  (:use [neko.activity :only [defactivity set-content-view!]]
+        [neko.threading :only [on-ui]]
+        [neko.ui :only [make-ui]]))
+
+(defactivity org.stuff.clojuroid.SubActivity
+  :def a
+  :on-create
+  (fn [this bundle]
+    (on-ui
+     (set-content-view! a
+      (make-ui [:linear-layout {}
+                [:text-view {:text "sub activity"}]])))))
